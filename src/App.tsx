7a8827fd8f23b442d083d99acefb49542be8bf94
“Olga <strelod@yandex.ru>
2023-10-22 17:23:40 -0700

    feat: btns

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React, { createElement as e, useState } from 'react'
 import { Product } from './components/Product'
 import { products } from './data/products'
+import { IProduct } from './models'
 
 function App() {
+    // const productsMarkup: void[] = products.map((product) => {
+    //     <Product key={product.id} product={product} />
+    // })
     return (
         <div className="container mx-auto max-w-2xl pt-5">
-            <Product product={products[0]} />
+            {/* {productsMarkup()} */}
+            <Product key={1} product={products[0]} />
+            <Product key={2} product={products[1]} />
+
         </div>
     )
 }
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,11 +1,30 @@
+import { useState } from 'react'
 import { IProduct } from '../models'
 interface ProductProps {
+    key: number
     product: IProduct
 }
-export function Product(props: ProductProps) {
+export function Product({ product }: ProductProps) {
+    const [details, setDetails] = useState(false)
+
+    const btnClassName = details ? 'bg-blue-400' : 'bg-yellow-400'
+    const btnClasses = ['py-2 px-4 border rounded', btnClassName]
     return (
         <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
-            {props.product.category}
+            <img src={product.image} alt={product.title} className="w-1/6" />
+            <p>{product.title}</p>
+            <p className="font-bold"> {product.price}</p>
+            <button
+                onClick={() => setDetails(!details)}
+                className={btnClasses.join(' ')}
+            >
+                {details ? 'Hide Details' : 'Show Details'}
+            </button>
+            {details && (
+                <div className="">
+                    <p>{product.description}</p>
+                </div>
+            )}
         </div>
     )
 }
