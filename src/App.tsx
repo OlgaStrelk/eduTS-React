dbb6aadaddbf1f1ddd0d4497f33ac0b94a65e311
“Olga <strelod@yandex.ru>
2023-10-23 07:52:01 -0700

    feat: fetching products

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React, { createElement as e, useState } from 'react'
+import React, { createElement as e, useEffect, useState } from 'react'
 import { Product } from './components/Product'
-import { products } from './data/products'
 import { IProduct } from './models'
+// import { products } from './data/products'
 
 function App() {
+    const [products, setProducts] = useState<IProduct[]>([])
+    const PRODUCTS_URL = 'https://fakestoreapi.com/products'
+    const getMovies = () => {
+        return fetch(PRODUCTS_URL, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+        }).then((res) => (res.ok ? res.json() : Promise.reject(res.status)))
+    }
+    useEffect(() => {
+        getMovies().then((items) => {
+            setProducts(items)
+        })
+    }, [])
+
     const productsMarkup = products.map((product) => (
         <Product key={product.id} product={product} />
     ))
